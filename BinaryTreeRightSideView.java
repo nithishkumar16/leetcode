8ccff0811e77aa0b0839b36221bf121f706795d8
nithish <nithish6@gmail.com>
2025-05-26 12:14:54 -0700

    BinaryTreeRightSideView

---
+++ BinaryTreeRightSideView.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class BinaryTreeRightSideView {
+    List<Integer> res = new ArrayList<>();
+    
+    public List<Integer> rightSideView(TreeNode root) {
+        dfs(root, 0);
+        return res;
+    }
+    
+    private void dfs(TreeNode node, int depth) {
+        if (node == null) {
+            return;
+        }
+        
+        if (res.size() == depth) {
+            res.add(node.val);
+        }
+        
+        dfs(node.right, depth + 1);
+        dfs(node.left, depth + 1);
+    }
+}
